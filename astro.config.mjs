c1307b183b1a7db4b1b3eb4d852e00b9a3999335
wihanneethling-hub <wihan.neethling@gmail.com>
2026-06-07 13:10:28 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://thetechteacher.co.za",
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "github-light"
